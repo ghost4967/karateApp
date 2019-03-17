4cc3b59a5ddc4f4efeb1dfc15771aaea4e5c77b5
wanda <wanda.agp14@gmail.com>
2019-03-17 18:37:43 -0400

    Validation for join to sesion

--- KarateApp/Source/src/pages/join-sesion/join-sesion.ts
+++ KarateApp/Source/src/pages/join-sesion/join-sesion.ts
@@ -14,6 +14,8 @@ export class JoinSesionPage {
     judgeName: '',
     sessionName: ''
   };
+  error: string;
+  errorName: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private karateService: KarateService) {
  
@@ -24,11 +26,32 @@ export class JoinSesionPage {
   }
 
   joinToSession() {
-    this.karateService.joinToPanel(this.data);
-    this.navCtrl.push('WaitingKataPage',{
-      sessionName: this.data.sessionName,
-      judgeName: this.data.judgeName
-    });
+    if(this.data.sessionName.length != 0 && this.data.judgeName.length != 0) {
+      this.karateService.getPanelName(this.data.sessionName).subscribe(dataSesion => {
+        if (!dataSesion) {
+          this.error = 'El panel no existe por favor intente de nuevo.';
+          this.errorName = '';
+        }
+      })
+      this.karateService.judgeExist(this.data.sessionName, this.data.judgeName).subscribe(dataJudge => {
+         if (dataJudge){
+           console.log(dataJudge);
+           this.error = '';
+           this.errorName = 'El juez ya existe por favor increse otro';
+         } else {
+           this.karateService.joinToPanel(this.data);
+           console.log("Guardandojuez");
+            this.navCtrl.push('WaitingKataPage',{
+            sessionName: this.data.sessionName,
+            judgeName: this.data.judgeName
+          });
+        }
+      })
+     } 
+    else {
+      this.errorName = 'El nombre es requerido por favor intente de nuevo.';
+      this.error = 'El panel es requerido por favor intente de nuevo.';
+    }
   }
 
 }
