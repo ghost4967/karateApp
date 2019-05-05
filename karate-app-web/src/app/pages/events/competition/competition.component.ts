1f1ad970163984e85ecf8afdfbf5524604c70e23
Andy <andy.camacho@fundacion-jala.org>
2019-05-05 16:47:14 -0400

    refactor and add draw option

--- karate-app-web/src/app/pages/events/competition/competition.component.ts
+++ karate-app-web/src/app/pages/events/competition/competition.component.ts
@@ -7,6 +7,7 @@ import { CompetitionService } from '../../../services/competition-service/compet
 import { OfflineCompetitor } from '../../../models/offline-competitor';
 import { Competitor } from '../../../models/competitor';
 import { Subscription } from 'rxjs';
+import { GroupsFilterPipe } from '../../../pipes/groups/groups-filter.pipe';
 
 
 @Component({
@@ -28,6 +29,9 @@ export class CompetitionComponent implements OnInit {
   sesion: string;
   readyToFinal: boolean = false;
   competition: FirebaseCompetition = new FirebaseCompetition();
+  drawGroup: Group;
+  drawCompetitors: Array<any> = new Array();
+  startDrawGrading: boolean;
 
   constructor(private route: ActivatedRoute, private sortService: SortService, private competitionService: CompetitionService) {
     this.eventId = route.snapshot.paramMap.get('eventId');
@@ -111,7 +115,10 @@ export class CompetitionComponent implements OnInit {
       subscription = this.competitionService.getGrade(group.kataManager).subscribe(val => {
         if (val != null) {
           offlineCompetitor.grade = val;
-          this.competitionService.createCompetitorGrade(offlineCompetitor, val, group.kata);
+          console.log(this.drawCompetitors);
+          if (!this.startDrawGrading) {
+            this.competitionService.createCompetitorGrade(offlineCompetitor, val, group.kata);
+          }
           offlineCompetitor.isGradePresent = true;
           offlineCompetitor.inGradingProcess = false;
           subscription.unsubscribe();
@@ -123,6 +130,8 @@ export class CompetitionComponent implements OnInit {
 
   nextKata(group: Group) {
     group.competitors.sort((c1, c2) => c2['grade'] - c1['grade']);
+    this.startDrawGrading = false;
+    this.drawCompetitors = [];
     let qualifiedCompetitors = group.competitors.slice(0, 4);
     let nextKata = group.kata - 1;
     let nextGroup = this.competition.groups.find(group => group.kata == nextKata && group.side == this.side);
@@ -134,6 +143,7 @@ export class CompetitionComponent implements OnInit {
       delete competitor['qualified'];
       delete competitor['isGradePresent']
     });
+    qualifiedCompetitors.reverse();
     nextGroup.competitors = qualifiedCompetitors;
     if (group.isGraded) {
       let searchGroup = this.competition.groups.find(g => g.kata == group.kata && g.side == group.side);
@@ -147,8 +157,10 @@ export class CompetitionComponent implements OnInit {
     offlineCompetitor.restartGrading = true;
     offlineCompetitor.grade = null;
   }
+
   orderGrades(group: Group) {
     group.competitors.sort((c1, c2) => c2['grade'] - c1['grade']);
+    let repeatedGrades = this.foundRepeatedGrades(group.competitors);
     if (group.kata == 2) {
       group.competitors.slice(0, 3).forEach(competitor => {
         competitor['qualified'] = true;
@@ -157,11 +169,42 @@ export class CompetitionComponent implements OnInit {
       searchGroup.isGraded = true;
       this.competitionService.updateCompetitionById(this.competition);
     } else {
-      group.competitors.slice(0, 4).forEach(competitor => {
-        competitor['qualified'] = true;
+      group.competitors.forEach(competitor => {
+        if (repeatedGrades.find(element => element == competitor['grade']) && group.competitors.slice(0,5).some(c => c['grade'] == competitor['grade'])) {
+          competitor['hasRepeatedGrade'] = true;
+          competitor['qualified'] = true;
+        } else if (group.competitors.slice(0,4).some(c => c['grade'] == competitor['grade'])) {
+          competitor['qualified'] = true;
+        }
       });
     }
+  }
 
+  addToDrawGroup(offlineCompetitor, group) {
+    this.startDrawGrading = true;
+    let index = group.competitors.findIndex(competitor => competitor == offlineCompetitor);
+    console.log(index);
+    offlineCompetitor['index'] = index;
+    this.drawCompetitors.push(offlineCompetitor);
+    console.log(this.drawCompetitors);
+    this.restartCompetitorCompetition(offlineCompetitor);
+  }
 
+  private foundRepeatedGrades(competitors) {
+    const object = {};
+    const result = [];
+
+    competitors.forEach(item => {
+      if(!object[item['grade']])
+          object[item['grade']] = 0;
+        object[item['grade']] += 1;
+    })
+
+    for (const prop in object) {
+       if(object[prop] >= 2) {
+           result.push(prop);
+       }
+    }
+    return result;
   }
 }
