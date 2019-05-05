aa429eec87fd255fc6c7b40ae233208ef142e6af
DanyAraya <danyaraya60@gmail.com>
2019-05-04 22:21:33 -0400

    creation of router method

--- karate-app-web/src/app/pages/events/competitor-register/competitor-register.component.ts
+++ karate-app-web/src/app/pages/events/competitor-register/competitor-register.component.ts
@@ -5,7 +5,7 @@ import { EventService } from '../../../services/event-service/event.service';
 import { Competitor } from '../../../models/competitor';
 import { Event } from '../../../models/event';
 import { CompetitorService } from '../../../services/competitor-service/competitor.service';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ngx-competitor-register',
@@ -19,10 +19,10 @@ export class CompetitorRegisterComponent implements OnInit {
   categories: Categorie[];
   competitor: Competitor = new Competitor();
 
-  constructor(private route: ActivatedRoute, private eventService: EventService, private competitorService: CompetitorService,
+  constructor(private activatedRoute: ActivatedRoute, private eventService: EventService, private competitorService: CompetitorService,
     private formBuilder: FormBuilder, private router: Router) {
-    this.eventId = route.snapshot.paramMap.get('eventId');
-    this.countryId = route.snapshot.paramMap.get('countryId');
+    this.eventId = activatedRoute.snapshot.paramMap.get('eventId');
+    this.countryId = activatedRoute.snapshot.paramMap.get('countryId');
    }
 
   ngOnInit() {
@@ -43,14 +43,28 @@ export class CompetitorRegisterComponent implements OnInit {
 
   buildCompetitorForm() {
     return this.formBuilder.group({
-      
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      secondLastName: [''],
+      birthDate: [''],
+      ci: [''],
+      gender: ['', [Validators.required]],
+      categorie: ['', [Validators.required]],
+      eventId: this.eventId,
+      countryId: this.countryId
+
     });
+  }
+
+  goToAddCountries() {
+    return this.router.navigate([`/pages/events/${this.eventId}/add-countries/${this.countryId}`]);
   }
 
   registerCompetitor() {
     this.competitor.eventId = this.eventId;
     this.competitor.countryId = this.countryId;
     this.competitorService.createCompetitor(this.competitor);
+    this.goToAddCountries();
   }
 
 }
