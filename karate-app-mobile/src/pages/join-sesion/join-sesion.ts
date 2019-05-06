dc45a85d310273a3c4915177458ffe7ff7649ba1
Jared Achi <jared.achi@fundacion-jala.org>
2019-05-05 20:50:55 -0400

    added judges validation

--- karate-app-mobile/src/pages/join-sesion/join-sesion.ts
+++ karate-app-mobile/src/pages/join-sesion/join-sesion.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { KarateService } from '../../services/karate.service';
+import { Observable } from 'rxjs';
 
 @IonicPage()
 @Component({
@@ -17,17 +18,17 @@ export class JoinSesionPage {
   error: string;
   errorName: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private karateService: KarateService, 
+  constructor(public navCtrl: NavController, public navParams: NavParams, private karateService: KarateService,
     private alertController: AlertController) {
- 
-}
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad JoinSesionPage');
   }
 
   joinToSession() {
-    if(this.data.sessionName.length != 0 && this.data.judgeName.length != 0) {
+    if (this.data.sessionName.length != 0 && this.data.judgeName.length != 0) {
       this.karateService.getPanelName(this.data.sessionName).subscribe(dataSesion => {
         if (!dataSesion) {
           this.error = 'El panel no existe por favor intente de nuevo.';
@@ -35,20 +36,26 @@ export class JoinSesionPage {
         }
       })
       this.karateService.judgeExist(this.data.sessionName, this.data.judgeName).subscribe(dataJudge => {
-         if (dataJudge){
-           console.log(dataJudge);
-           this.error = '';
-           this.errorName = 'El juez ya existe por favor ingrese otro';
-         } else {
-           this.karateService.joinToPanel(this.data);
-           console.log("Guardandojuez");
-            this.navCtrl.setRoot('WaitingKataPage',{
-            sessionName: this.data.sessionName,
-            judgeName: this.data.judgeName
+        if (dataJudge) {
+          console.log(dataJudge);
+          this.error = '';
+          this.errorName = 'El juez ya existe por favor ingrese otro';
+        } else {
+          this.hasSpaceForANewJudge().subscribe(hasSpace => {
+            if (hasSpace) {
+              this.karateService.joinToPanel(this.data);
+              console.log("Guardandojuez");
+              this.navCtrl.setRoot('WaitingKataPage', {
+                sessionName: this.data.sessionName,
+                judgeName: this.data.judgeName
+              });
+            } else {
+              this.alertNoSpace();
+            }
           });
         }
       })
-     } 
+    }
     else {
       this.errorName = 'El nombre es requerido por favor intente de nuevo.';
       this.error = 'El panel es requerido por favor intente de nuevo.';
@@ -77,7 +84,40 @@ export class JoinSesionPage {
       ]
     });
     await alert.present();
+    //getByName, getNumberOfJudges
+  }
 
-}
+  async alertNoSpace() {
+    const alert = await this.alertController.create({
+      title: 'Panel lleno',
+      message: 'El panel al que se desea unir se encuentra lleno',
+      buttons: [
+        {
+          text: 'ok',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }
+      ]
+    });
+    await alert.present();
+    //getByName, getNumberOfJudges
+  }
+
+  hasSpaceForANewJudge() {
+    return new Observable(observer => {
+      var judgesNumber, judgesList;
+      this.karateService.getNumberOfJudges(this.data.sessionName).subscribe(data => {
+        judgesNumber = data;
+        this.karateService.getByName(this.data.sessionName).subscribe(data => {
+          judgesList = data;
+          observer.next(judgesList.length < judgesNumber);
+          observer.complete();
+        });
+      })
+    })
+  }
 
 }
