bea4df4480d592a214a694a7e18945a60ae08cff
DanyAraya <danyaraya60@gmail.com>
2019-05-07 23:31:09 -0400

    create timer for the app mobile

--- karate-app-mobile/src/pages/waiting-kata-manager/waiting-kata-manager.module.ts
+++ karate-app-mobile/src/pages/waiting-kata-manager/waiting-kata-manager.module.ts
@@ -1,13 +1,21 @@
+import { MbscModule } from '@mobiscroll/angular';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { WaitingKataManagerPage } from './waiting-kata-manager';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     WaitingKataManagerPage,
   ],
-  imports: [
+  imports: [ 
+    MbscModule,
     IonicPageModule.forChild(WaitingKataManagerPage),
+    FormsModule, 
+    ReactiveFormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
   ],
 })
 export class WaitingKataManagerPageModule {}
